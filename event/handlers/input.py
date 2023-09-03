c93714cad941b147ef477fca87c092e2f8eec118
Tanvir Hasan Akash <th.akash0047@gmail.com>
2023-09-03 13:12:22 +0600

    Fixed #1
    Changed input.py and window.py to follow the tab indentation styling.

--- event/handlers/input.py
+++ event/handlers/input.py
@@ -5,10 +5,10 @@ from event.event_handlers import EventHandler
 
 
 class InputEventHandler(EventHandler):
-    def handle_event(self, event: pg.event) -> None:
-        self.__handle_quit_event(event)
+	def handle_event(self, event: pg.event) -> None:
+		self.__handle_quit_event(event)
 
-    def __handle_quit_event(self, event: pg.event) -> None:
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
+	def __handle_quit_event(self, event: pg.event) -> None:
+		if event.type == pg.QUIT:
+			pg.quit()
+			sys.exit()
--- event/handlers/window.py
+++ event/handlers/window.py
@@ -6,24 +6,24 @@ from window.window_manager import WindowManager
 
 
 class WindowEventHandler(EventHandler):
-    def __init__(self, window: WindowManager) -> None:
-        self.window = window
+	def __init__(self, window: WindowManager) -> None:
+		self.window = window
 
-    def handle_event(self, event: pg.event) -> None:
-        self.__handle_resize_event(event)
+	def handle_event(self, event: pg.event) -> None:
+		self.__handle_resize_event(event)
 
-    def __handle_resize_event(self, event: pg.event) -> None:
-        if event.type == pg.VIDEORESIZE:
-            if event.w < self.window.min_width:
-                event.w = self.window.min_width
-            if event.h < self.window.min_height:
-                event.h = self.window.min_height
+	def __handle_resize_event(self, event: pg.event) -> None:
+		if event.type == pg.VIDEORESIZE:
+			if event.w < self.window.min_width:
+				event.w = self.window.min_width
+			if event.h < self.window.min_height:
+				event.h = self.window.min_height
 
-            # Uncomment to check for maximum width or height
-            # if self.window.max_width is not None and event.w > self.window.max_width:
-            #     event.w = self.window.max_width
-            # if self.window.max_height is not None and event.h > self.window.max_height:
-            #     event.h = self.window.max_height
+			# Uncomment to check for maximum width or height
+			# if self.window.max_width is not None and event.w > self.window.max_width:
+			#     event.w = self.window.max_width
+			# if self.window.max_height is not None and event.h > self.window.max_height:
+			#     event.h = self.window.max_height
 
-            self.window.width, self.window.height = event.w, event.h
-            self.window.screen = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
+			self.window.width, self.window.height = event.w, event.h
+			self.window.screen = pg.display.set_mode((event.w, event.h), pg.RESIZABLE)
